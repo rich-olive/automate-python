4490cb8768ac6e08a4cf22274e2c1bb768621187
olivia <oliviam.richmond@gmail.com>
2025-02-20 15:35:31 +0000

    added if/else to manage file existence

--- paths/crazy_paths.py
+++ paths/crazy_paths.py
@@ -6,7 +6,16 @@ print(crazy_path)
 # C:\Users\olivi\I\dont\exist.csv
 # it looks like this file and path actually exist
 
-with open(crazy_path, "r") as file:
-    print(file.read())
+# with open(crazy_path, "r") as file:
+#     print(file.read())
 # but when we try to open it:
 # FileNotFoundError: [Errno 2] No such file or directory: 'C:\\Users\\olivi\\I\\dont\\exist.csv'
+
+if crazy_path.exists():
+    with open(crazy_path, "r") as file:
+        print(file.read())
+else:
+    print("That file does not exist")
+
+# use exist() method to verify that the file exists
+# exist() returns a boolean
