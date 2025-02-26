f1d02a07f029a9bdac514eab9599e11c6177d05d
olivia <oliviam.richmond@gmail.com>
2025-02-26 02:37:27 +0000

    reviewed solution video

--- regular_expressions/ssn_mini_project.py
+++ regular_expressions/ssn_mini_project.py
@@ -24,9 +24,22 @@ def anonymise_ssn(ssn: str):
     # .group to isolate groups (000)-(00)-(0000)
     # .sub to replace the numbers with ***
 
-anonymise_ssn("321-54-9876") # valid
-anonymise_ssn("321-54-876") # invalid, too few digits
-anonymise_ssn("321-549876") # invalid, missing hyphen
-anonymise_ssn("321/54-9876") # invalid, slash instead of hyphen
-anonymise_ssn("3a1-54-9876") # invalid, letter instead of number
-anonymise_ssn("321-54") # invalid, too few digits -> this is a separate case
+# anonymise_ssn("321-54-9876") # valid
+# anonymise_ssn("321-54-876") # invalid, too few digits
+# anonymise_ssn("321-549876") # invalid, missing hyphen
+# anonymise_ssn("321/54-9876") # invalid, slash instead of hyphen
+# anonymise_ssn("3a1-54-9876") # invalid, letter instead of number
+# anonymise_ssn("321-54") # invalid, too few digits -> this is a separate case
+
+
+# SOLUTION
+
+def mask_ssn(text):
+    regex = regex_valid_ssn = r"\d{3}-\d{2}-(\d{4})"
+    # creates only one group in the regex string: (\d{4})
+    clean_ssns = re.sub(regex, r"***-**-\1", text)
+    # back referencing in the replace string: \1 - targets that one and only group
+    return clean_ssns
+
+sample_text = "An example of a valid SSN is 321-54-9876. However, 321-45 is NOT a valid SSN."
+print(mask_ssn(sample_text)) # An example of a valid SSN is ***-**-9876. However, 321-45 is NOT a valid SSN.
